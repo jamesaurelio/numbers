7cf07f14df3cad661e7ffa1c656a83261fab90f5
jamesaurelio <jamesemmanueloaurelio@iskolarngbayan.pup.edu.ph>
2024-03-30 12:24:23 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            while (i <= 100)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Hello Goodbye");
+                        i++;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Hello");
+                        i++;
+                    }
+                    
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                    i++;
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            }
+        }
+    }
+}
